f9d04b917adfa32ae5fc48eafb3f7e3d3d6bfde4
nud3l <dominik.harz@gmail.com>
2015-10-16 22:17:52 +0200

    Updated login and DB assignments

---
+++ DBScripts/items.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO items(
+            oid, title, caption, image)
+    VALUES (1, 'Delft', 'delft_trees', 'http://us.123rf.com/450wm/jankranendonk/jankranendonk1207/jankranendonk120700050/14636319-canal-with-trees-and-the-leaning-tower-of-the-old-church-of-delft.jpg'), (2, 'Den Haag', 'den haag_trees', 'https://s-media-cache-ak0.pinimg.com/736x/a1/3f/61/a13f615d70cb52f7e814abc6ebe7858d.jpg'), (3, 'Amsterdam', 'amsterdam_trees', 'http://leafywalks.com/amsterdam/_img/lang_trees_amsterdam_caucwing.jpg');
